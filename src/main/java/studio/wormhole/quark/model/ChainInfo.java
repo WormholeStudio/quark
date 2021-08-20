4e40e8b85651c59f87f0c9f583f0b13e38d430ef
antares <antares@wormhole.studio>
2021-08-20 23:09:31 +0800

    add mobius_dev

--- src/main/java/studio/wormhole/quark/model/ChainInfo.java
+++ src/main/java/studio/wormhole/quark/model/ChainInfo.java
@@ -11,9 +11,9 @@ public enum ChainInfo {
     DEV("local", "http://127.0.0.1:9850", 254),
     HALLEY("halley", "https://halley-seed.starcoin.org", 253),
     BARNARD("barnard", "https://barnard-seed.starcoin.org", 251),
-    PROXIMA("proxima", "https://proxima-seed.starcoin.org", 2),
-    MAIN("main", "https://main-seed.starcoin.org", 1);
-
+    PROXIMA("proxima", "https://proxima-seed.starcoin.org", 252),
+    MAIN("main", "https://main-seed.starcoin.org", 1),
+    MOBIUS_DEV("mobius_dev", "http://39.106.68.120:9850", 66);
     private String name;
     private String url;
     private int chainId;
